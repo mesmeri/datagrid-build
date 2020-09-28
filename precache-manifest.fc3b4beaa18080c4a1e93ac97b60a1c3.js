47e9b8a47faed881e8cf4a20e0f2f2b211e53077
mesmeri <49590226+mesmeri@users.noreply.github.com>
2020-09-28 09:46:36 +0300

    Add files via upload

---
+++ precache-manifest.fc3b4beaa18080c4a1e93ac97b60a1c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981476064cdbc99bf6206a7de3191ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9c942355c4bda45edc",
+    "url": "/static/css/main.60132a5f.chunk.css"
+  },
+  {
+    "revision": "a932886ddccb4ec765d3",
+    "url": "/static/js/2.a966e058.chunk.js"
+  },
+  {
+    "revision": "ebf314d28da7b1544af5821296fdb154",
+    "url": "/static/js/2.a966e058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9c942355c4bda45edc",
+    "url": "/static/js/main.38947782.chunk.js"
+  },
+  {
+    "revision": "707d41e56b4e984fb3b1",
+    "url": "/static/js/runtime-main.335ad2b1.js"
+  }
+]);
